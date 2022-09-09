1b4fc4bd852b873f26f846093cc6ea6f8d756dd3
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-09-09 08:46:23 +0200

    created a search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<div className="container">
+				<header className="App-header"></header>
+				<main>
+					<Dictionary />
+				</main>
+				<footer className="App-footer">
+					{" "}
+					<small>Coded by Malgorzata Stano</small>
+				</footer>
+			</div>
 		</div>
 	);
 }
