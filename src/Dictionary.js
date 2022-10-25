d68af01fee77a8906ac22ca5b29e2b5260edb490
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-10-25 19:09:51 +0200

    API integration is working properly

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,33 @@
+
+
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-	let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
 
-	function handleResponse(response) {
-		console.log(response.data[0]);
-	}
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
 
-	function search(event) {
-		event.preventDefault();
+  function search(event) {
+    event.preventDefault();
 
-		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/sunset${keyword}`;
-	axios.get(apiUrl).then(handleResponse);
-	}
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-	function handleKeywordChange(event) {
-		setKeyword(event.target.value);
-	}
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-	return (
-		<div className="Dictionary">
-			<form onSubmit={search}>
-				<input type="search" autoFocus={true} onChange={handleKeywordChange} />
-			</form>
-		</div>
-	);
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
