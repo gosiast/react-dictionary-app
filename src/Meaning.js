df82715dab3b66f8d2ce88627f90b00d1981e7fc
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-10-25 20:56:59 +0200

    synonyms work

--- src/Meaning.js
+++ src/Meaning.js
@@ -12,10 +12,9 @@ export default function Meaning (props) {
         return (
             <div key={index}>
             <p>
-                <strong>Definition: </strong>
+             <strong>Definition: </strong>
             {definition.definition}
             <br/>
-            
         <strong>Example: </strong>         
            <em>{definition.example}</em> 
             <Synonyms synonyms={definition.synonyms} />
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,13 +3,15 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
+      <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
-      </ul>
+      </div>
     );
   } else {
     return null;
   }
 }
+
+
