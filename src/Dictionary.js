6b9684ed92ccdf9b3eb3ec9e87b9edd80ef893d5
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-10-26 09:58:02 +0200

    updated CSS - added default keyword

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,36 +3,54 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] =useState(false);
 
   function handleResponse(response) {
     console.log(response.data[0]);
 	setResults(response.data[0]);
 }
 
-  function search(event) {
-    event.preventDefault();
-
-    // documentation: https://dictionaryapi.dev/e
+function search() {
+ // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+}
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
+function load() {
+  setLoaded(true);
+  search();
+}
+
+if (loaded) {
+return (
     <div className="Dictionary">
       <section>
-      <form onSubmit={search}>
+      <form onSubmit={handleSubmit}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <div className="hint">
+        suggested words: yoga, forest, music, dancing...
+      </div>
             </section>
 
 	  <Results results={results}/>
     </div>
   );
+} else {
+  load();
+
+}
+
+  
 }
