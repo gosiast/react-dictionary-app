6e199574604357bdc3137e385f9a4ae1d797cacb
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-10-26 08:35:59 +0200

    phonetics is working

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning (props) {
-    console.log(props.meaning);
     return (
     <div className="Meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
@@ -17,8 +16,8 @@ export default function Meaning (props) {
             <br/>
         <strong>Example: </strong>         
            <em>{definition.example}</em> 
-            <Synonyms synonyms={definition.synonyms} />
             </p>
+            <Synonyms synonyms={definition.synonyms} />
             </div>
         );
         })}
