e6361f301d0a970f19572e47c7717813d9ff93f0
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-10-26 10:07:29 +0200

    fixed styling of Phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactAudioPlayer from 'react-audio-player';
+import './Phonetic.css';
 
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
+      <span className="phonetic-text">
+      {props.phonetic.text}
+      </span>
+      <div className="audio-player">
         <ReactAudioPlayer
   src={props.phonetic.audio}
   autoPlay={false}
   controls
 />
-      <br />
-      {props.phonetic.text}
+</div>
     </div>
   );
 }
