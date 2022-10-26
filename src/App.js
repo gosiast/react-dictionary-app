a629ab7633aee1b8b0083addeb8ce66269b33061
Malgorzata Stano <stano.malgorzata@gmail.com>
2022-10-26 10:26:49 +0200

    updated the footer with gitHub links

--- src/App.js
+++ src/App.js
@@ -9,9 +9,26 @@ function App() {
 				<main>
 					<Dictionary defaultKeyword="happiness"/>
 				</main>
-				<footer className="App-footer">
-					<small>Coded by Malgorzata Stano</small>
-				</footer>
+			<footer className="App-footer">
+				This project was coded by{" "}
+				<a
+					href="https://github.com/gosiast"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{" "}
+					Malgorzata Stano{" "}
+				</a>
+				and is
+				<a
+					href="https://github.com/gosiast/react-dictionary-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{" "}
+					open sourced{" "}
+				</a>{" "}
+			</footer>
 			</div>
 		</div>
 	);
